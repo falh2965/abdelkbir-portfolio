59f7009607621ad80a6952697c2f2c3923ba132f
falh2965 <kabirfalh@gmail.com>
2025-12-11 01:52:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll for the "Learn More" button
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
